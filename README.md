88d40601353edc839d5308f75ff34ff9cfc7687f
Rodrigo Cipriani da Rosa <chopsss@gmail.com>
2018-11-25 20:17:25 -0200

    Code clean

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hardmob
+
 Hardmob promotions
+
+Simple test to access some URL and extract information from HTML to transforme in API.
+
+`It is only an test, be free to use`
--- hardmobService.js
+++ hardmobService.js
@@ -1,9 +1,5 @@
-// Just run `node extractor.js`
-
 const axios = require("axios");
 const cheerio = require("cheerio");
-const qs = require("qs");
-const fs = require("fs");
 
 async function getPromotions() {
   try {
@@ -29,40 +25,10 @@ async function getPromotions() {
         .children();
       const name = el.text();
       const href = `${urlPrefix}${el[1].attribs["href"]}`;
-      //   console.log("###");
-      //   console.log("###", `${urlPrefix}${el[1].attribs["href"]}`);
       products.push({ name, href });
     });
 
-    console.log("products", products);
     return products;
-
-    // $("#produtos > tbody > tr").each(function(i, elem) {
-    //   const code = $(this)
-    //     .children()
-    //     .eq(0)
-    //     .first()
-    //     .text()
-    //     .trim();
-    //   const name = $(this)
-    //     .children()
-    //     .eq(1)
-    //     .first()
-    //     .text()
-    //     .trim();
-    //   const group = $(this)
-    //     .children()
-    //     .eq(2)
-    //     .first()
-    //     .text()
-    //     .trim();
-    //   codes.push({
-    //     code,
-    //     name,
-    //     group
-    //   });
-    // });
-    // return codes;
   } catch (error) {
     console.error(error);
   }
