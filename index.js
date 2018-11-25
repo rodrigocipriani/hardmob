c62649db6caa8faf52c5cf3304742c4c1b50e3f5
Rodrigo Cipriani da Rosa <chopsss@gmail.com>
2018-11-25 19:51:53 -0200

    Change server

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const hardmobService = require("./hardmobService");
 
-const PORT = 3000;
-
 const app = express();
+
+app.set("port", process.env.PORT || 80);
 
 app.get("/", async (req, res) => {
   const promotions = await hardmobService.getPromotions();
   res.json(promotions);
 });
 
-app.listen(PORT, function() {
-  console.log(`Server running on port ${PORT}`);
+app.listen(app.get("port"), function() {
+  console.log("Node app is running at localhost:" + app.get("port"));
 });
